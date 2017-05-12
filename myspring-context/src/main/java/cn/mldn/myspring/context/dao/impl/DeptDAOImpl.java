a14c54a5a6cff117c6446a61c146a9e51d51aa22
'lans' <'nalnus@qq.com'>
2017-05-12 09:48:29 +0800

    Annotation注入：Component, Repository, Service, Controller, Resource

--- myspring-context/src/main/java/cn/mldn/myspring/context/dao/impl/DeptDAOImpl.java
+++ myspring-context/src/main/java/cn/mldn/myspring/context/dao/impl/DeptDAOImpl.java
@@ -1,8 +1,10 @@
 package cn.mldn.myspring.context.dao.impl;
 
+import org.springframework.stereotype.Repository;
+
 import cn.mldn.myspring.context.dao.IDeptDAO;
 import cn.mldn.myspring.context.vo.Dept;
-
+@Repository
 public class DeptDAOImpl implements IDeptDAO {
 
 	@Override
--- myspring-context/src/main/java/cn/mldn/myspring/context/service/impl/DeptServiceImpl.java
+++ myspring-context/src/main/java/cn/mldn/myspring/context/service/impl/DeptServiceImpl.java
@@ -1,14 +1,17 @@
 package cn.mldn.myspring.context.service.impl;
 
+import javax.annotation.Resource;
+
+import org.springframework.stereotype.Service;
+
 import cn.mldn.myspring.context.dao.IDeptDAO;
 import cn.mldn.myspring.context.service.IDeptService;
 import cn.mldn.myspring.context.vo.Dept;
-
+@Service
 public class DeptServiceImpl implements IDeptService {
+	@Resource
 	private IDeptDAO deptDAO ;
-	public void setDeptDAO(IDeptDAO deptDAO) {
-		this.deptDAO = deptDAO;
-	}
+	
 	@Override
 	public boolean add(Dept vo) {
 		System.out.println("**********【IDeptService业务层：数据增加】**********");
